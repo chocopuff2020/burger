912fa191fb9c8e1329c8c55ea6a4e8d26f2dc161
Michelle Dai <michelledai2020@gmail.com>
2017-05-07 18:24:13 -0700

    Some done

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+
+// Setup up MySql Connection
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "ASDFAAAA",
+    database: "burgers_db"
+  });
+}
+
+// Test Connection
+connection.connect();
+
+// export connection for ORM
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers_db.burgers (burger_name) values ("Unicorn Burger");
+INSERT INTO burgers_db.burgers (burger_name) values ("Unicorn Cheese Burger");
+INSERT INTO burgers_db.burgers (burger_name) values ("Unicorn ham Burger");
+INSERT INTO burgers_db.burgers (burger_name,devoured) values ("Unicorn poop Burger", 1);
